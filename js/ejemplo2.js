4dd9bc85444690f44ab22653169bc390f09a52a9
CarlosRR19 <carlosrr19@educastur.es>
2023-09-25 14:05:39 +0200

    ejercicio 2 update

--- js/ejemplo2.js
+++ js/ejemplo2.js
@@ -1,54 +1,43 @@
 /**
  * DWEC: Ejemplo 2
  * @author Maria R.F.
- * 
- * Realiza una nueva versión del programa anterior donde en primer lugar se lea el número de nombres que vamos a introducir 
- * (habrá que validar usando la función parseInt() que se trata de un número entero). 
- * 
- * A continuación se leerán tantos nombres como se ha especificado y se mostrarán en una tabla como en el ejercicio anterior. 
- * Por último se pedirá un nombre al usuario, para el cual se quiere averiguar su posición en el array. 
- * 
+ *
+ * Realiza una nueva versión del programa anterior donde en primer lugar se lea el número de nombres que vamos a introducir
+ * (habrá que validar usando la función parseInt() que se trata de un número entero).
+ *
+ * A continuación se leerán tantos nombres como se ha especificado y se mostrarán en una tabla como en el ejercicio anterior.
+ * Por último se pedirá un nombre al usuario, para el cual se quiere averiguar su posición en el array.
+ *
  * Se informará al usuario de la posición que ocupa el nombre o, en caso de no estar, del error correspondiente.
  */
- 
- /* Encontrar los errores del siguiente script, usando el depurador de Google Chrome */
 
+/* Encontrar los errores del siguiente script, usando el depurador de Google Chrome */
 
+var nombres = [];
 
-var nombres=[];
+var numeroNombres = null;
 
-var numeroNombres=NaN; 
+var nombreBuscado = "",
+  encontrado = false;
 
+while (numeroNombres == null) {
+  numeroNombres = parseInt(prompt("¿Cuántos nombres vas a leer?", "5"));
+}
 
-var nombreBuscado="",encontrado=false;
+for (var i = 0; i < numeroNombres; i++) {
+  nombres[i] = prompt("Introduce un nombre");
+}
 
+window.alert(nombres);
 
+nombreBuscado = prompt("¿Qué nombre deseas buscar?");
 
+for (var i = 0; i < nombres.length; i++) {
+  if ((nombres[i] == nombreBuscado)) {
+    encontrado = true;
+    alert(nombreBuscado + " está en la posición " + i);
+  }
 
- while(numeroNombres==NaN){
-	numeroNombres=parseInt(prompt("¿Cuántos nombres vas a leer?","10"));
- }
-   
+}
 
-
-for (var i=0;i<numeroNombres;i++){
-	nombres[i]=prompt("Introduce un nombre","Agapito ");
-} 
-
-
-nombreBuscado=prompt("¿Qué nombre deseas buscar?","");
-
-
-
-for (var i=0;i<nombres.length;i++){
-	if(nombres[i]=nombre_buscado){
-		encontrado=true;
-		alert(nombreBuscado+" está en la posición "i);
-	
-} 
-
-if(!encontrado)
-		prompt("No se encuentra el nombre buscado");
-		
-
-
+if (!encontrado) window.alert("No se encuentra el nombre buscado");
